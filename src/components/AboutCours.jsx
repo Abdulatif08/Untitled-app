8174e8a3066929e9e552e01603d4ac94ab092c86
sunnatullo54 <sultanovural28@gmail.com>
2025-06-20 12:10:15 +0500

    AboutCours

--- src/components/AboutCours.jsx
+++ src/components/AboutCours.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const AboutCours = () => {
   return (
-    <div>
-      
+    <div className='bg-[#1E1E20] pt-[60px] pb-[60px]'>
+      <div className='container w-[900px]'>
+        <h1 className='text-[#FFFFFF] text-[36px] leading-[100%] text-center'>Sizda hozir <span className='text-[#2249E7]'>harakat qilish kerak</span> degan tushuncha mavjud, ammo aniq nima qilish kerakligini bilmaysiz</h1>
+      </div>
     </div>
   )
 }
