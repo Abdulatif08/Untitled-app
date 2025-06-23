05c034e71ce8f208a79e7ba58ce79320dc08d3b9
javlonbek013 <nomonov@gmail.com>
2025-06-23 15:45:07 +0500

    added free course section

--- src/components/FreeCourse.jsx
+++ src/components/FreeCourse.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const FreeCourse = () => {
   return (
-    <div>
-      
+    <div className="container free-course">
+      <div className="free-left">
+        <h1>BEPUL DARSGA<br />RO‘YXATDAN  O‘TING</h1>
+        <p>Va o‘zингizning ingliz tili darajangizni<br />aniqlash imkoniyatiga ega bo‘ling</p>
+      </div>
+      <div className="free-right">
+        <input type="text" placeholder="Ismingiz" />
+        <input type="text" placeholder="Telefon raqamingiz" />
+        <input type="text" placeholder="Telefon raqamingiz" />
+        <button>BEPUL DARSGA YOZILISH</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FreeCourse
+export default FreeCourse;
